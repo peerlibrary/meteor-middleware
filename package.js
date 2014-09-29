aae7bc0e0c8adad795c97d1fda5ec28f34b962c1
Mitar <mitar.git@tnode.com>
2014-09-28 21:58:41 -0700

    Migration to Meteor 0.9.0.

--- package.js
+++ package.js
@@ -1,8 +1,12 @@
 Package.describe({
-  summary: "Middleware support for Meteor publish functions"
+  summary: "Middleware support for Meteor publish functions",
+  version: '0.1.0',
+  name: 'peerlibrary:middleware',
+  git: 'https://github.com/peerlibrary/meteor-middleware.git'
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom('METEOR@0.9.3');
   api.use(['coffeescript', 'underscore'], 'server');
 
   api.export('PublishEndpoint');
@@ -14,7 +18,7 @@ Package.on_use(function (api) {
 });
 
 Package.on_test(function (api) {
-  api.use(['middleware', 'tinytest', 'test-helpers', 'coffeescript', 'insecure', 'random', 'assert', 'underscore'], ['client', 'server']);
+  api.use(['peerlibrary:middleware', 'tinytest', 'test-helpers', 'coffeescript', 'insecure', 'random', 'peerlibrary:assert', 'underscore'], ['client', 'server']);
 
   api.add_files('tests.coffee', ['client', 'server']);
 });
